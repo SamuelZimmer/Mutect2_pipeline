4b751676dec03349385f15561884c45e4c8b68f4
Samuel Zimmer <zimmers@ip29.m>
2019-10-23 11:40:41 -0400

    Removing some useless files

--- up_to_date/mp2b/cleanup.sh
+++ up_to_date/mp2b/cleanup.sh
@@ -12,37 +12,36 @@ PREVIOUS=$2
 OUTPUT_DIR=`pwd`
 JOB_OUTPUT_DIR=$OUTPUT_DIR/job_output
 cd $JOB_OUTPUT_DIR
-mkdir -p $OUTPUT_DIR/jobs
 
-JOB_DEPENDENCIES=$(cat ${JOB_OUTPUT_DIR}/${PREVIOUS}/${NOPATHNAME}.JOBID)
 
-mkdir -p $OUTPUT_DIR/logs
+JOB_DEPENDENCIES=$(sacct --format="JobID,JobName%60" | grep ${NOPATHNAME} | cut -d ' ' -f 1 | sed ':a;N;$!ba;s/\n/,/g')
 
-# Define a timestamp function
-timestamp() {
-  date +"%Y-%m-%d %H:%M:%S"
-}
+
 
 JOB1="cd $OUTPUT_DIR
+
+
+
+
+mkdir -p $OUTPUT_DIR/jobs
+mkdir -p $OUTPUT_DIR/logs
 mv $JOB_OUTPUT_DIR/*.out $OUTPUT_DIR/logs/
 mv $OUTPUT_DIR/*.out $OUTPUT_DIR/logs/
 mv $JOB_OUTPUT_DIR/*/*.log $OUTPUT_DIR/logs/
 mv $JOB_OUTPUT_DIR/*.log $OUTPUT_DIR/logs/
-
 mv $JOB_OUTPUT_DIR/*/*.sh $OUTPUT_DIR/jobs/
 
 #getting all the job informations using seff
 echo -e 'PATIENT\tSTEP\tJOBID' > $OUTPUT_DIR/JOBIDS.list
-for i in `ls job_output/*/*.JOBID`; do STEP=`echo $i | cut -d "/" -f 2`; PATIENT=`echo $i | cut -d "/" -f 3 | cut -d "_" -f 1` ; JOBID=`cat $i`; echo -e $PATIENT'\t'$STEP'\t'$JOBID; done >> $OUTPUT_DIR/JOBIDS.list
-for i in `ls job_output/*/*/*.JOBID`; do STEP=`echo $i | cut -d "/" -f 2`; PATIENT=`echo $i | cut -d "/" -f 3 | cut -d "_" -f 1` ; JOBID=`cat $i`; echo -e $PATIENT'\t'$STEP'\t'$JOBID; done >> $OUTPUT_DIR/JOBIDS.list
+for i in `ls job_output/*/*.JOBID`; do STEP=`echo $i | cut -d '/' -f 2`; PATIENT=`echo $i | cut -d '/' -f 3 | cut -d '_' -f 1` ; JOBID=`cat $i`; echo -e $PATIENT'\t'$STEP'\t'$JOBID; done >> $OUTPUT_DIR/JOBIDS.list
+for i in `ls job_output/*/*/*.JOBID`; do STEP=`echo $i | cut -d '/' -f 2`; PATIENT=`echo $i | cut -d '/' -f 3 | cut -d '_' -f 1` ; JOBID=`cat $i`; echo -e $PATIENT'\t'$STEP'\t'$JOBID; done >> $OUTPUT_DIR/JOBIDS.list
+
 rm $JOB_OUTPUT_DIR/*/*.JOBID
 rm $JOB_OUTPUT_DIR/*/*/*.JOBID
 
 #for i in `tail -n +2 JOBIDS.list | cut -f 3`; do echo $i; seff $i; done
 
-#mv $JOB_OUTPUT_DIR/Sambamba_markDuplicates/*.ba* /home/zimmers/ip29_home/Preprocessed/MarkDup
-
-mv $JOB_OUTPUT_DIR/Recalibration/*.ba* /home/zimmers/ip29_home/Mutect2/recalibrated/
+mv $JOB_OUTPUT_DIR/Sambamba_markDuplicates/*.ba* /home/zimmers/projects/def-jacquesp/zimmers/bam_files/Sambamba_markDuplicates/
 
 rm -fr $JOB_OUTPUT_DIR/ReplaceReadGroup $JOB_OUTPUT_DIR/Sambamba_markDuplicates $JOB_OUTPUT_DIR/FixMate
 "
@@ -57,5 +56,5 @@ echo "$COMMAND" >> $OUTPUT_DIR/jobs/cleanup.sh
 
 
 sbatch --job-name=Cleanup_${NOPATHNAME} --output=%x-%j.out --time=24:00:00 --mem=2G \
---dependency=afterok:$JOB_DEPENDENCIES $OUTPUT_DIR/jobs/cleanup.sh \
+--dependency=afterok:$JOB_DEPENDENCIES $OUTPUT_DIR/jobs/cleanup.sh
 
