3939f229f1fe21fabb089ab180c4e9e16fd7c83d
Samuel Zimmer <zimmers@ip29.m>
2019-10-25 12:07:24 -0400

    Reevalutating use on Belluga for recalibration step

--- Belluga/test/make_chromosome_list.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-COUNTER=1 ; while [ $COUNTER -lt 23 ] ; do echo "chr"$COUNTER >> chromosome.list ; let COUNTER=$COUNTER+1; done
-echo "chrX" >> chromosome.list
-echo "chrY" >> chromosome.list
-echo "chrMT" >> chromosome.list
-
--- Belluga/test/seff.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-JOBID=$1
-OUT=$2
-
-seff $JOBID >> $OUT
